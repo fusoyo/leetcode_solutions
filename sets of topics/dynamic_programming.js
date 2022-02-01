f3b620a56b7ef01e4d1f0d1a98c874a2e9abee77
fusoyo <fengchuyao20@gmail.com>
2022-02-02 00:07:06 +0900

    2022-02-01

--- sets of topics/dynamic_programming.js
+++ sets of topics/dynamic_programming.js
@@ -40,6 +40,34 @@ const memoFib = (n, memo = {}) => {
 // console.log(memoFib(7))
 
 
-
 //----------------------------------------------------------------------------------------------------------------------
-// grid traveler.
+// grid traveler. (0,1) (1,0) mean empty grid; (3,3) means from (0,0) moves to (3,3);  more like tree sum problem.
+const gridTraveler = (m, n) => {
+    if (m === 1 && n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+
+    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+}
+//brute force: O(2**(n+m)) time, O(n+m) space;
+// console.log(gridTraveler(3,3))
+// console.log(gridTraveler(3,2))  //3
+// console.log(gridTraveler(2,3))  //3
+// console.log(gridTraveler(18,18))
+
+// cuz the duplicate of the subtree, so make a memo, which enhances the efficiency;
+const gridTravelerMemo = (m, n, memo = {}) => {
+    const key = m + ',' + n;
+    if (key in memo) return memo[key]
+    if (m === 1 && n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+
+    memo[key] = gridTravelerMemo(m - 1, n, memo) + gridTravelerMemo(m, n - 1, memo);
+    return memo[key]
+
+}
+//memorized O(m*n) time, o(n+m) space.
+console.log(gridTravelerMemo(3, 2))  //3
+console.log(gridTravelerMemo(18,18))
+
+
+
